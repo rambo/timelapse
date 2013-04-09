5a53852abcdd167a64a438569a5267ecad4bb200
Eero af Heurlin <rambo@iki.fi>
2013-04-09 21:53:25 +0300

    started writing a deamon, realized I need a service, twisted should help AFAIRC

---
+++ README.md
@@ -0,0 +1,9 @@
+Motion timelapse
+================
+
+Idea is to have [motion][motionweb] monitoring the space and when event starts a python daemon
+to handle [capturing][captureweb] the timelapse photos is started.
+
+[motionweb]: http://www.lavrsen.dk/foswiki/bin/view/Motion
+[captureweb]: http://capture.sourceforge.net/
+
---
+++ capture_service.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import signal, daemon
+import sys, os, os.path
+import time
+
+# Daemon config
+config = {
+    'images_dir': os.path.expanduser(os.path.join('~', 'timelapse_dumps')),
+    'max_shots': 50, # How many shots max take before restarting the camera
+    'max_tiume': 1200, # How many seconds max to keep the camera on without restarting
+}
+
+
+class capture(object):
+    pass
+
+
+class capture_service(object):
+    pass
+
+    def initialize(self):
+        pass
+
+    def reload(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    # Initialize Daemon context
+    context = daemon.DaemonContext(
+        working_directory=config['images_dir'],
+        umask=0o002,
+        pidfile=lockfile.FileLock(os.path.join(config['images_dir'], 'capture_service.pid')),
+    )
+
+    s = capture_service()
+    
+    context.signal_map = {
+        signal.SIGTERM: s.stop,
+        signal.SIGHUP: s.reload,
+        signal.SIGUSR1: s.reload,
+    }
+
+    if (len(sys.argv) < 2):
+        print "Use 'start' or 'stop' as argument'"
+        sys.exit(1)
+
+    if (sys.argv[1] == 'start'):
+        # TODO: check if already running
+        s.initialize()
+        with context:
+            s.main()
+        sys.exit(0)
+
+    if (sys.argv[1] == 'stop'):
+        # TODO: check if running
+        s.stop()
+
+    print "Unknown command '%s'" % sys.argv[1]
+    sys.exit(1)
