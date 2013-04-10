4a21c93ab81e26674d4fa12ede9232218d957d58
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-04-10 22:49:00 +0300

    what is going wrong here, this one will not even keep in the background now...

--- test_daemon.py
+++ test_daemon.py
@@ -6,16 +6,53 @@ import signal, daemon, lockfile
 import sys, os, os.path, syslog
 import time
 
+syslog.openlog(logoption=syslog.LOG_PID)
 
 def main():
+    i = 0
+    print 'main called'
     while True:
-        print "foo"
+        i += 1
+        print 'loop %d\n' % i
+        with open('test_daemon_out.txt', 'w') as f:
+            f.write('loop %d\n' % i)
         time.sleep(5)
 
+class streamproxy:
+    def open(self):
+        pass
+    
+    def close(self):
+        pass
+    
+    def write(self, msg):
+        pass
+    
+    def read(self):
+        return 0x0    
+
+class stdout(streamproxy):
+    def write(self, msg):
+         syslog.syslog(msg)
+
+class stderr(streamproxy):
+    def write(self, msg):
+         syslog.syslog(syslog.LOG_ERR, msg)
+    
+so = stdout()
+so.write('stdout proxy test')
+
+se = stderr()
+se.write('stderr proxy test')
 
 context = daemon.DaemonContext(
-    stdout=syslog.syslog,
-    stderr=lambda msg: syslog.syslog(syslog.LOG_ERR, msg)
+# This won't work the callbacks need to look like file objects so a wrapper is needed
+#    stdout=syslog.syslog,
+#    stderr=lambda msg: syslog.syslog(syslog.LOG_ERR, msg)
+    stdout=so,
+    stderr=se
 )
 with context:
     main()
+
+
