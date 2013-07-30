7d4fd4babe0b368352cfd008b664ad6bf4f6293a
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-07-30 20:45:26 +0300

    finalized the rename script

--- README.md
+++ README.md
@@ -28,10 +28,13 @@ will then convert these photos into a time lapse movie.
     mkdir resized
     mogrify -path resized/ -resize x1080 -gravity southwest -stroke '#000C' -strokewidth 5 -annotate 0 '%t' -stroke  none   -fill white    -annotate 0 '%t' `find . -name *.jpg`
 
+And if you do not have RTC on the computer you are using you might get weird timestamps, use [timestamp_adjust.py](./timestamp_adjust.py) to fix the names.
+
 ## Faster resize on multicore systems
 
 Use [GNU Parallel][gnuparallel].
 
+    mkdir resized
     find . -name *.jpg | parallel mogrify -path resized/ -resize x1080 -gravity southwest -stroke '\#000C' -strokewidth 5 -annotate 0 '%t' -stroke  none   -fill white    -annotate 0 '%t'
 
 [gnuparallel]: http://www.gnu.org/software/parallel/
--- timestamp_adjust.py
+++ timestamp_adjust.py
@@ -16,8 +16,12 @@ if __name__ == '__main__':
     files = args[1:]
     td = datetime.timedelta(days=int(adays), hours=int(ahours), minutes=int(aminutes))
     
-    #print td
-    #print files
-    
-    
-    
+    for fname in files:
+        fname_b = os.path.basename(fname)
+        file_time = datetime.datetime(year=int(fname_b[0:4]),month=int(fname_b[4:6]),day=int(fname_b[6:8]),hour=int(fname_b[9:11]),minute=int(fname_b[11:13]),second=int(fname_b[13:15]))
+        new_time = file_time+td
+        #print "DEBUG: adjusted %s to %s" % (file_time, new_time)
+        newname_b = "%s%s" % (new_time.strftime('%Y%m%d_%H%M%S'), fname_b[15:])
+        newname = fname.replace(fname_b, newname_b)
+        print "Renaming %s -> %s" % (fname, newname)
+        os.rename(fname, newname)
